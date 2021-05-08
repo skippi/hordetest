4a1f08f36998ff8fc8640b8a04ec89a438a2d4f0
skippi <jbtcao@gmail.com>
2021-05-08 00:14:54 -0500

    skeleton: add shooting without line of sight

--- src/main/java/io/github/skippi/hordetest/HordeTestPlugin.java
+++ src/main/java/io/github/skippi/hordetest/HordeTestPlugin.java
@@ -4,13 +4,24 @@ import com.comphenix.protocol.ProtocolLibrary;
 import com.comphenix.protocol.ProtocolManager;
 import com.destroystokyo.paper.event.entity.ProjectileCollideEvent;
 import org.bukkit.Bukkit;
+import org.bukkit.Location;
+import org.bukkit.World;
 import org.bukkit.block.Block;
+import org.bukkit.entity.Entity;
+import org.bukkit.entity.LivingEntity;
 import org.bukkit.entity.Player;
 import org.bukkit.entity.Skeleton;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.entity.EntitySpawnEvent;
 import org.bukkit.event.entity.ProjectileHitEvent;
 import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.scheduler.BukkitRunnable;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.Comparator;
+import java.util.Optional;
 
 public class HordeTestPlugin extends JavaPlugin implements Listener {
     private static ProtocolManager PM;
@@ -43,5 +54,52 @@ public class HordeTestPlugin extends JavaPlugin implements Listener {
         if (block == null) return;
         getBlockHealthManager().damage(block, 0.25);
         event.getEntity().remove();
+    }
+
+    @EventHandler
+    private void skeletonAlwaysFire(EntitySpawnEvent event) {
+        @NotNull Entity entity = event.getEntity();
+        if (!(entity instanceof Skeleton)) return;
+        Skeleton skeleton = (Skeleton) entity;
+        BukkitRunnable task = new BukkitRunnable() {
+            int cooldown = 0;
+
+            @Override
+            public void run() {
+                if (skeleton.isDead()) {
+                    cancel();
+                    return;
+                }
+                @Nullable LivingEntity target = skeleton.getTarget();
+                if (target == null || skeleton.hasLineOfSight(target)) {
+                    skeleton.setAI(true);
+                } else {
+                    skeleton.setAI(skeleton.getLocation().distance(target.getLocation()) > 20);
+                    --cooldown;
+                    if (cooldown <= 0) {
+                        skeleton.rangedAttack(target, 1);
+                        cooldown = 30;
+                    }
+                }
+            }
+        };
+        task.runTaskTimer(this, 0, 1);
+    }
+
+    @EventHandler
+    private void skeletonAcquire(EntitySpawnEvent event) {
+        @NotNull Entity entity = event.getEntity();
+        if (!(entity instanceof Skeleton)) return;
+        Skeleton skeleton = (Skeleton) entity;
+        new BukkitRunnable() {
+            @Override
+            public void run() {
+                getNearestPlayer(skeleton.getWorld(), skeleton.getLocation()).ifPresent(skeleton::setTarget);
+            }
+        }.runTaskTimer(this, 0, 2);
+    }
+
+    private static Optional<Player> getNearestPlayer(World world, Location origin) {
+        return world.getPlayers().stream().min(Comparator.comparing(p -> p.getLocation().distance(origin)));
     }
 }
