12c72793913a23b286a4b4ccf0f3090c39072a49
skippi <jbtcao@gmail.com>
2021-05-20 22:06:23 -0500

    add stage 4

--- src/main/java/io/github/skippi/hordetest/HordeTestPlugin.java
+++ src/main/java/io/github/skippi/hordetest/HordeTestPlugin.java
@@ -111,6 +111,16 @@ public class HordeTestPlugin extends JavaPlugin implements Listener {
                 if (horde.stream().filter(e -> e.getType().equals(EntityType.SKELETON)).count() < 6) {
                     genHostileSpawnLocation(player).ifPresent(loc -> horde.add(world.spawn(loc, Skeleton.class)));
                 }
+            } else if (STAGE == 3) {
+                if (horde.stream().filter(e -> e.getType().equals(EntityType.ZOMBIE)).count() < 16) {
+                    genHostileSpawnLocation(player).ifPresent(loc -> horde.add(world.spawn(loc, Zombie.class)));
+                }
+                if (horde.stream().filter(e -> e.getType().equals(EntityType.SKELETON)).count() < 6) {
+                    genHostileSpawnLocation(player).ifPresent(loc -> horde.add(world.spawn(loc, Skeleton.class)));
+                }
+                if (horde.stream().filter(e -> e.getType().equals(EntityType.SPIDER)).count() < 6) {
+                    genHostileSpawnLocation(player).ifPresent(loc -> horde.add(world.spawn(loc, Spider.class)));
+                }
             } else {
                 if (horde.stream().filter(e -> e.getType().equals(EntityType.ZOMBIE)).count() < 16) {
                     genHostileSpawnLocation(player).ifPresent(loc -> horde.add(world.spawn(loc, Zombie.class)));
@@ -120,6 +130,9 @@ public class HordeTestPlugin extends JavaPlugin implements Listener {
                 }
                 if (horde.stream().filter(e -> e.getType().equals(EntityType.SPIDER)).count() < 6) {
                     genHostileSpawnLocation(player).ifPresent(loc -> horde.add(world.spawn(loc, Spider.class)));
+                }
+                if (horde.stream().filter(e -> e.getType().equals(EntityType.CREEPER)).count() < 2) {
+                    genHostileSpawnLocation(player).ifPresent(loc -> horde.add(world.spawn(loc, Creeper.class)));
                 }
             }
         }
