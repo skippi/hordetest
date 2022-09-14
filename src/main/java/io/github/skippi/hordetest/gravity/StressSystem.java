47c7af7bbe55e2e7ff4debf10076bd6ba544a0b9
skippi <jbtcao@gmail.com>
2022-09-14 02:33:41 -0500

    optimize constant folding in isPermanentlyStable()
    JVM doesn't seem to fold getType() calls for some reason, so we're
    explicitly labeling the type as final here.

--- src/main/java/io/github/skippi/hordetest/gravity/StressSystem.java
+++ src/main/java/io/github/skippi/hordetest/gravity/StressSystem.java
@@ -132,20 +132,21 @@ public class StressSystem {
   }
 
   public static boolean isPermanentlyStable(Block block) {
-    return block.isLiquid()
-        || block.getType() == Material.OAK_LEAVES
-        || block.getType() == Material.SPRUCE_LEAVES
-        || block.getType() == Material.BIRCH_LEAVES
-        || block.getType() == Material.JUNGLE_LEAVES
-        || block.getType() == Material.ACACIA_LEAVES
-        || block.getType() == Material.DARK_OAK_LEAVES
-        || block.getType() == Material.MANGROVE_LEAVES
-        || block.getType() == Material.AZALEA_LEAVES
-        || block.getType() == Material.BEDROCK
-        || block.getType() == Material.FIRE
-        || block.getType() == Material.WALL_TORCH
-        || block.getType() == Material.REDSTONE_WALL_TORCH
-        || block.getType() == Material.SOUL_WALL_TORCH;
+    if (block.isLiquid()) return true;
+    final var type = block.getType();
+    return type == Material.OAK_LEAVES
+        || type == Material.SPRUCE_LEAVES
+        || type == Material.BIRCH_LEAVES
+        || type == Material.JUNGLE_LEAVES
+        || type == Material.ACACIA_LEAVES
+        || type == Material.DARK_OAK_LEAVES
+        || type == Material.MANGROVE_LEAVES
+        || type == Material.AZALEA_LEAVES
+        || type == Material.BEDROCK
+        || type == Material.FIRE
+        || type == Material.WALL_TORCH
+        || type == Material.REDSTONE_WALL_TORCH
+        || type == Material.SOUL_WALL_TORCH;
   }
 
   public boolean isBaseable(Block block) {
